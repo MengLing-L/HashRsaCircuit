abb0d33a33aeb6d2e1ba11198e098ac5bda31d5f
ubuntu <ubuntu@localhost.localdomain>
2021-11-06 13:45:43 +0800

    hash rsa

---
+++ JsnarkCircuitBuilder/src/examples/generators/rsahash/RSAEncryptionHashCircuitGenerator.java
@@ -0,0 +1,155 @@
+/*******************************************************************************
+ * Author: Ahmed Kosba <akosba@cs.umd.edu>
+ *******************************************************************************/
+package examples.generators.rsahash;
+
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+
+import circuit.auxiliary.LongElement;
+import circuit.eval.CircuitEvaluator;
+import circuit.structure.CircuitGenerator;
+import circuit.structure.Wire;
+import circuit.structure.WireArray;
+import examples.gadgets.rsa.RSAEncryptionV1_5_Gadget;
+import examples.gadgets.hash.SHA256Gadget;
+import examples.generators.rsa.RSAUtil;
+
+
+// a demo for RSA Encryption PKCS #1, V1.5
+public class RSAEncryptionHashCircuitGenerator extends CircuitGenerator {
+
+	private int rsaKeyLength;
+	private int plainTextLength;
+	private Wire[] inputMessage;
+	private Wire[] randomness;
+	private Wire[] cipherText;
+	private LongElement rsaModulus;
+
+	private SHA256Gadget sha2Gadget;
+	private RSAEncryptionV1_5_Gadget rsaEncryptionV1_5_Gadget;
+
+	public RSAEncryptionHashCircuitGenerator(String circuitName, int rsaKeyLength,
+			int plainTextLength) {
+		super(circuitName);
+		this.rsaKeyLength = rsaKeyLength;
+		this.plainTextLength = plainTextLength;
+		// constraints on the plaintext length will be checked by the gadget
+	}
+
+	@Override
+	protected void buildCircuit() {
+		inputMessage = createProverWitnessWireArray(plainTextLength); // in bytes
+		for(int i = 0; i < plainTextLength;i++){
+			inputMessage[i].restrictBitLength(8);
+		}
+		
+		rsaModulus = createLongElementInput(rsaKeyLength);
+		randomness = createProverWitnessWireArray(RSAEncryptionV1_5_Gadget
+				.getExpectedRandomnessLength(rsaKeyLength, plainTextLength));
+		rsaEncryptionV1_5_Gadget = new RSAEncryptionV1_5_Gadget(rsaModulus, inputMessage,
+				randomness, rsaKeyLength);
+		
+		// since randomness is a witness
+		rsaEncryptionV1_5_Gadget.checkRandomnessCompliance();
+		Wire[] cipherTextInBytes = rsaEncryptionV1_5_Gadget.getOutputWires(); // in bytes
+		cipherText = new WireArray(cipherTextInBytes).packWordsIntoLargerWords(8, 8);
+		System.out.println(cipherText.length);
+
+		Wire[] digest = new SHA256Gadget(inputMessage, 8, plainTextLength, false, true, "").getOutputWires();
+		
+		System.out.println(digest.length);
+		Wire[] hashCipherTextInBytes = new Wire[cipherText.length + digest.length];
+		System.arraycopy(cipherText, 0, hashCipherTextInBytes, 0, cipherText.length);
+		System.arraycopy(digest, 0, hashCipherTextInBytes, cipherText.length, digest.length);
+		//Wire[] hashCipherTextInBytes = new WireArray(cipherTextInBytes).addWireArray(new WireArray(msgHashBytes), msgHashBytes.length).getBits(32).packBitsIntoWords(8);
+		System.out.println(hashCipherTextInBytes.length);
+		// group every 8 bytes together
+		makeOutputArray(hashCipherTextInBytes,
+				"Output hash and cipher text");
+
+	}
+
+	@Override
+	public void generateSampleInput(CircuitEvaluator evaluator) {
+
+		String msg = "";
+		for (int i = 0; i < inputMessage.length; i++) {
+
+			evaluator.setWireValue(inputMessage[i], (int) ('a' + i));
+			msg = msg + (char) ('a' + i);
+		}
+		System.out.println("PlainText:" + msg);
+
+		try {
+
+			// to make sure that the implementation is working fine,
+			// encrypt with the underlying java implementation for RSA
+			// Encryption in a sample run,
+			// extract the randomness (after decryption manually), then run the
+			// circuit with the extracted randomness
+
+			SecureRandom random = new SecureRandom();
+			KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+			generator.initialize(rsaKeyLength, random);
+			KeyPair pair = generator.generateKeyPair();
+			Key pubKey = pair.getPublic();
+			BigInteger modulus = ((RSAPublicKey) pubKey).getModulus();
+
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			evaluator.setWireValue(this.rsaModulus, modulus,
+					LongElement.CHUNK_BITWIDTH);
+
+			Key privKey = pair.getPrivate();
+
+			cipher.init(Cipher.ENCRYPT_MODE, pubKey, random);
+			byte[] cipherText = cipher.doFinal(msg.getBytes());
+//			System.out.println("ciphertext : " + new String(cipherText));
+			byte[] cipherTextPadded = new byte[cipherText.length + 1];
+			System.arraycopy(cipherText, 0, cipherTextPadded, 1, cipherText.length);
+			cipherTextPadded[0] = 0;
+
+			byte[][] result = RSAUtil.extractRSARandomness1_5(cipherText,
+					(RSAPrivateKey) privKey);
+			// result[0] contains the plaintext (after decryption)
+			// result[1] contains the randomness
+
+			boolean check = Arrays.equals(result[0], msg.getBytes());
+			if (!check) {
+				throw new RuntimeException(
+						"Randomness Extraction did not decrypt right");
+			}
+
+			byte[] sampleRandomness = result[1];
+			for (int i = 0; i < sampleRandomness.length; i++) {
+				evaluator.setWireValue(randomness[i], (sampleRandomness[i]+256)%256);
+			}
+
+		} catch (Exception e) {
+			System.err
+					.println("Error while generating sample input for circuit");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		int keyLength = 1024;
+		int msgLength = 3;
+		RSAEncryptionHashCircuitGenerator generator = new RSAEncryptionHashCircuitGenerator(
+				"rsahash" + keyLength + "_hashencryption", keyLength, msgLength);
+		generator.generateCircuit();
+		generator.evalCircuit();
+		generator.prepFiles();
+		generator.runLibsnark();
+	}
+
+}
---
+++ JsnarkCircuitBuilder/src/examples/tests/rsahash/HashRSAEncryption_Test.java
@@ -0,0 +1,202 @@
+/*******************************************************************************
+ * Author: Mengling LIU <mengling@connect.hku.hk>
+ *******************************************************************************/
+package examples.tests.rsahash;
+
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import circuit.auxiliary.LongElement;
+import circuit.eval.CircuitEvaluator;
+import circuit.structure.CircuitGenerator;
+import circuit.structure.Wire;
+import circuit.structure.WireArray;
+import examples.gadgets.rsa.RSAEncryptionV1_5_Gadget;
+import examples.generators.rsa.RSAUtil;
+import examples.gadgets.hash.SHA256Gadget;
+import util.Util;
+
+
+public class HashRSAEncryption_Test extends TestCase {
+
+	
+	@Test
+	public void testHashEncryption() throws Exception{
+
+		
+		String plainText = "abc";
+		//String inputStr = "abc";
+		String expectedDigest = "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad";
+
+		
+
+		int keySize = 1024;
+
+		final byte[] cipherTextBytes = new byte[keySize/8];			
+		SecureRandom random = new SecureRandom();
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(keySize, random);
+		KeyPair keyPair = keyGen.generateKeyPair();
+		Key pubKey = keyPair.getPublic();
+		BigInteger rsaModulusValue = ((RSAPublicKey) pubKey).getModulus();
+		
+		
+		CircuitGenerator generator = new CircuitGenerator("HashRSA" + keySize
+				+ "_Enc_TestHashEncryption") {
+
+			 int rsaKeyLength = keySize;
+			 int plainTextLength = plainText.length();
+			 Wire[] inputMessage;
+			 Wire[] randomness;
+			 Wire[] cipherText;
+			 LongElement rsaModulus;
+
+			 RSAEncryptionV1_5_Gadget rsaEncryptionV1_5_Gadget;
+
+			@Override
+			protected void buildCircuit() {
+				inputMessage = createProverWitnessWireArray(plainTextLength); // in bytes
+				for(int i = 0; i < plainTextLength;i++){
+					inputMessage[i].restrictBitLength(8);
+				}
+				
+				rsaModulus = createLongElementInput(rsaKeyLength);
+				randomness = createProverWitnessWireArray(RSAEncryptionV1_5_Gadget
+						.getExpectedRandomnessLength(rsaKeyLength, plainTextLength));
+				rsaEncryptionV1_5_Gadget = new RSAEncryptionV1_5_Gadget(rsaModulus, inputMessage,
+						randomness, rsaKeyLength);
+				
+				// since randomness is a witness
+				rsaEncryptionV1_5_Gadget.checkRandomnessCompliance();
+				Wire[] cipherTextInBytes = rsaEncryptionV1_5_Gadget.getOutputWires(); // in bytes
+				cipherText = new WireArray(cipherTextInBytes).packWordsIntoLargerWords(8, 8);
+				System.out.println(cipherText.length);
+
+				Wire[] digest = new SHA256Gadget(inputMessage, 8, plainText.length(), false, true, "").getOutputWires();
+				/*Wire[] msgHashBytes = new WireArray(digest).getBits(32).packBitsIntoWords(8);
+				for (int i = 0; i < 8; i++) {
+					Wire tmp = msgHashBytes[4 * i];
+					msgHashBytes[4 * i] = msgHashBytes[(4 * i + 3)];
+					msgHashBytes[4 * i + 3] = tmp;
+					tmp = msgHashBytes[4 * i + 1];
+					msgHashBytes[4 * i + 1] = msgHashBytes[4 * i + 2];
+					msgHashBytes[4 * i + 2] = tmp;
+				}*/
+				System.out.println(digest.length);
+				Wire[] hashCipherTextInBytes = new Wire[cipherText.length + digest.length];
+				System.arraycopy(cipherText, 0, hashCipherTextInBytes, 0, cipherText.length);
+				System.arraycopy(digest, 0, hashCipherTextInBytes, cipherText.length, digest.length);
+				//Wire[] hashCipherTextInBytes = new WireArray(cipherTextInBytes).addWireArray(new WireArray(msgHashBytes), msgHashBytes.length).getBits(32).packBitsIntoWords(8);
+				System.out.println(hashCipherTextInBytes.length);
+				// group every 8 bytes together
+				makeOutputArray(hashCipherTextInBytes,
+						"Output hash and cipher text");
+			}
+
+			@Override
+			public void generateSampleInput(CircuitEvaluator evaluator) {
+
+				for (int i = 0; i < inputMessage.length; i++) {
+					evaluator.setWireValue(inputMessage[i],
+							plainText.charAt(i));
+				}
+				try {
+					Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+					evaluator.setWireValue(this.rsaModulus, rsaModulusValue,
+							LongElement.CHUNK_BITWIDTH);
+
+					Key privKey = keyPair.getPrivate();
+
+					cipher.init(Cipher.ENCRYPT_MODE, pubKey, random);
+					byte[] tmp = cipher.doFinal(plainText.getBytes());
+					System.arraycopy(tmp, 0, cipherTextBytes, 0, keySize/8);
+					
+					byte[] cipherTextPadded = new byte[cipherTextBytes.length + 1];
+					System.arraycopy(cipherTextBytes, 0, cipherTextPadded, 1, cipherTextBytes.length);
+					cipherTextPadded[0] = 0;
+
+					byte[][] result = RSAUtil.extractRSARandomness1_5(cipherTextBytes,
+							(RSAPrivateKey) privKey);
+
+					boolean check = Arrays.equals(result[0], plainText.getBytes());
+					if (!check) {
+						throw new RuntimeException(
+								"Randomness Extraction did not decrypt right");
+					}
+
+					byte[] sampleRandomness = result[1];
+					for (int i = 0; i < sampleRandomness.length; i++) {
+						evaluator.setWireValue(randomness[i], (sampleRandomness[i]+256)%256);
+					}
+
+				} catch (Exception e) {
+					System.err
+							.println("Error while generating sample input for circuit");
+					e.printStackTrace();
+				}
+			}
+		};
+
+		generator.generateCircuit();
+		generator.evalCircuit();
+		CircuitEvaluator evaluator = generator.getCircuitEvaluator();
+		
+		// retrieve the ciphertext from the circuit, and verify that it matches the expected ciphertext and that it decrypts correctly (using the Java built-in RSA decryptor)
+		/*ArrayList<Wire> cipherTextList = generator.getOutWires();
+		BigInteger t = BigInteger.ZERO;
+		int i = 0;
+		for(Wire w:cipherTextList){
+			BigInteger val = evaluator.getWireValue(w);
+			t = t.add(val.shiftLeft(i*64));
+			i++;
+		}*/
+		ArrayList<Wire> wires = generator.getOutWires();
+		BigInteger t = BigInteger.ZERO;
+
+		for (int i=0; i< 16; i++){
+			BigInteger val = evaluator.getWireValue(wires.get(i));
+			t = t.add(val.shiftLeft(i*64));
+		}
+
+	
+		// extract the bytes
+		byte[] cipherTextBytesFromCircuit = t.toByteArray();
+
+		// ignore the sign byte if any was added
+		if(t.bitLength() == keySize && cipherTextBytesFromCircuit.length == keySize/8+1){
+			cipherTextBytesFromCircuit=Arrays.copyOfRange(cipherTextBytesFromCircuit, 1, cipherTextBytesFromCircuit.length);
+		}
+		
+		for(int k = 0; k < cipherTextBytesFromCircuit.length; k++){
+			assertEquals(cipherTextBytes[k], cipherTextBytesFromCircuit[k]);
+		}
+
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+		byte[] cipherTextDecrypted = cipher.doFinal(cipherTextBytesFromCircuit);
+		assertTrue(Arrays.equals(plainText.getBytes(), cipherTextDecrypted));
+
+		String outDigest = "";
+		for (int i=16; i< 24; i++) {
+			outDigest += Util.padZeros(evaluator.getWireValue(wires.get(i)).toString(16), 8);
+		}
+		assertEquals(outDigest, expectedDigest);
+		
+		
+		
+
+	}
+}
